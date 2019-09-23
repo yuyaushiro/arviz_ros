bd47d875d885a429d2607f867a5a76a7a37fc28d
yuyaushiro <yuya.suzuki0829@gmail.com>
2019-09-24 01:19:46 +0900

    Add a function to generate PointCloud from OccupancyGrid

--- src/coordinate_alignment_node.cpp
+++ src/coordinate_alignment_node.cpp
@@ -1,6 +1,8 @@
 #include <ros/ros.h>
+#include <tf/transform_listener.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl_ros/transforms.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <std_msgs/Empty.h>
 #include <nav_msgs/OccupancyGrid.h>
@@ -20,6 +22,8 @@ class CoordinateAlignment
   void AlignmentCallback(const std_msgs::EmptyConstPtr& data);
 
   void publishPointCloud(const pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud);
+  void convertMapIntoPointCloud(const nav_msgs::OccupancyGridConstPtr& occupancy_grid,
+                                pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud);
 
  private:
   // ノードハンドラ
@@ -30,6 +34,9 @@ class CoordinateAlignment
   ros::Subscriber alignment_sub_;
 
   ros::Publisher pointcloud_pub_;  // デバッグ用に点群のPub
+
+  // 点群
+  pcl::PointCloud<pcl::PointXYZ>::Ptr map_pc_;
 };
 
 CoordinateAlignment::CoordinateAlignment()
@@ -42,10 +49,14 @@ CoordinateAlignment::CoordinateAlignment()
                                                   &CoordinateAlignment::AlignmentCallback, this);
 
   pointcloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("pc_out", 1, true);
+
+  map_pc_ = pcl::PointCloud<pcl::PointXYZ>::Ptr(new pcl::PointCloud<pcl::PointXYZ>);
 }
 
 void CoordinateAlignment::mapCallback(const nav_msgs::OccupancyGridConstPtr& map)
 {
+  convertMapIntoPointCloud(map, map_pc_);
+  publishPointCloud(map_pc_);
 }
 
 void CoordinateAlignment::scanCallback(const sensor_msgs::LaserScanConstPtr& scan)
@@ -54,6 +65,47 @@ void CoordinateAlignment::scanCallback(const sensor_msgs::LaserScanConstPtr& sca
 
 void CoordinateAlignment::AlignmentCallback(const std_msgs::EmptyConstPtr& data)
 {
+}
+
+void CoordinateAlignment::convertMapIntoPointCloud(const nav_msgs::OccupancyGridConstPtr& occupancy_grid,
+                                                   pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud)
+{
+   // マップ情報
+  std::vector<int8_t> map_image = occupancy_grid->data;
+  float map_resolution = occupancy_grid->info.resolution;
+  int map_width = occupancy_grid->info.width;
+  
+  // resizeの為にカウント
+  size_t point_num = 0;  // 障害物格子の数
+  for (int pixel : map_image)
+    if (pixel >= 100) point_num++;
+
+  // 点群情報
+  point_cloud->width = point_num;
+  point_cloud->height = 1;
+  point_cloud->is_dense = false;
+  point_cloud->points.resize(point_cloud->width * point_cloud->height);  // 予めresize
+
+  //点群を画素の左下ではなく中心に生成するためのオフセット
+  float pixel_offset = map_resolution / 2;
+
+  // 障害物格子から点群を生成
+  size_t point_index = 0;  // 生成した点の数
+  for (int i = 0; i < std::end(map_image) - std::begin(map_image); i++)
+  {
+    if (map_image[i] >= 100)
+    {
+      point_cloud->points[point_index].x = (float)(i % map_width) * map_resolution + pixel_offset;
+      point_cloud->points[point_index].y = (float)(i / map_width) * map_resolution + pixel_offset;
+      point_cloud->points[point_index].z = 0.0;
+      point_index++;
+    }
+  }
+
+  // map座標系への座標変換
+  tf::Transform transform_map;
+  tf::poseMsgToTF(occupancy_grid->info.origin, transform_map);
+  pcl_ros::transformPointCloud(*point_cloud, *point_cloud, transform_map);
 }
 
 void CoordinateAlignment::publishPointCloud(const pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud)
